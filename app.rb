fb1f5a16caf45b8a235e0942c919c239cc4c4eda
jose luis <cortezluisjose@gmail.com>
2019-10-26 21:31:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-  
+  "My name is __" 
 end
 get '/' do
-  "Hello, World!"
+ 
 end
 get '/' do
   "Hello, World!"
