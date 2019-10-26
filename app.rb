3d79199e38ab5bc919a293511ecc3ca56f0009e2
jose luis <cortezluisjose@gmail.com>
2019-10-26 21:32:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ class App < Sinatra::Base
   get '/name' do
   "My name is __" 
 end
-get '/' do
+get '/hometown' do
  "My hometown is __"
 end
-get '/' do
-  
+get '/favorite-song' do
+  "My favorite song is __"
 end
 end
